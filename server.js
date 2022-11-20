1de5f6a08e0bb769e4ada9cd119a542d4a82e19d
Damir Gimatdinov <damirgim@yandex.ru>
2022-11-20 11:39:08 +0400

    Adds Model & MongoDB
    Adds initial Setup of Mongo\Atlas connection, 
    setup of cloud and deps.

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const { errorHandler } = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+
+connectDB();
 
 app.use(cors(corsOptions));
 
@@ -34,4 +39,14 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (error) => {
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
