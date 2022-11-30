edfb94643748855ca1816e8a55dcef6a666e276a
Damir Gimatdinov <damirgim@yandex.ru>
2022-11-30 17:23:16 +0400

    Corrected allowed origings

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "https://www.dgimatdinov.ru"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.dgimatdinov.ru",
+  "https://mernnotes.onrender.com",
+];
 
 module.exports = allowedOrigins;
